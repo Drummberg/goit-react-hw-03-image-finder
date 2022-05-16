87929004183c44c563618b5d99cd8c8e30eefef9
Drummberg <drummberg@gmail.com>
2022-05-16 16:54:03 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,15 @@
 import PulseLoader from 'react-spinners/PulseLoader';
-import PropTypes from 'prop-types';
 
 const wrapperStyles = {
   margin: 0,
 };
 
-const Loader = ({ enabled }) => {
+const Loader = () => {
   return (
     <div css={wrapperStyles}>
-      <PulseLoader margin={4} size={13} color={'#3f51b5'} enabled={enabled} />
+      <PulseLoader margin={4} size={13} color={'#3f51b5'} />
     </div>
   );
-};
-
-Loader.propTypes = {
-  enabled: PropTypes.bool.isRequired,
 };
 
 export default Loader;
